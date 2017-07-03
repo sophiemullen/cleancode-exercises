c7e80e4332492b62c68394b3fde6d7979e948bef
Sophie Mullen <sophie.mullen@cegeka.com>
2017-07-03 21:48:05 +0200

    Boolean arguments now work with Argument Marshaler

--- src/main/java/be/swsb/cleancode/ch14/args/Args.java
+++ src/main/java/be/swsb/cleancode/ch14/args/Args.java
@@ -8,7 +8,7 @@ public class Args {
     private String[] args;
     private boolean valid = true;
     private Set<Character> unexpectedArguments = new TreeSet<Character>();
-    private Map<Character, Boolean> booleanArgs = new HashMap<Character, Boolean>();
+    private Map<Character, ArgumentMarshaler> booleanArgs = new HashMap<Character, ArgumentMarshaler>();
     private Map<Character, String> stringArgs = new HashMap<Character, String>();
     private Map<Character, Integer> intArgs = new HashMap<Character, Integer>();
     private Set<Character> argsFound = new HashSet<Character>();
@@ -73,7 +73,7 @@ public class Args {
     }
 
     private void parseBooleanSchemaElement(char elementId) {
-        booleanArgs.put(elementId, false);
+        booleanArgs.put(elementId, new BooleanArgumentMarshaler());
     }
 
     private void parseIntegerSchemaElement(char elementId) {
@@ -179,7 +179,7 @@ public class Args {
     }
 
     private void setBooleanArg(char argChar, boolean value) {
-        booleanArgs.put(argChar, value);
+        booleanArgs.get(argChar).setBoolean(value);
     }
 
     private boolean isBooleanArg(char argChar) {
@@ -248,7 +248,8 @@ public class Args {
     }
 
     public boolean getBoolean(char arg) {
-        return falseIfNull(booleanArgs.get(arg));
+        Args.ArgumentMarshaler argumentMarshaler = booleanArgs.get(arg);
+        return argumentMarshaler.getBoolean();
     }
 
     public boolean has(char arg) {
